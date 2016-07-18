d361c5e98538d9e649cd1e77977ea9fea53910d6
szczypiorofix <szczypiorofix@o2.pl>
2016-07-18 22:46:13 +0200

    Poprawki w animacji BeeObject.

--- src/com/platformer/game/objects/BeeObject.java
+++ src/com/platformer/game/objects/BeeObject.java
@@ -17,32 +17,33 @@ private int moving = 1;
 private int startPos = 0;
 
 public BeeObject(ObjectId id, float x, float y) {
-	super(id, x, y, 128, 128);
+	super(id, x, y, 32, 32);
 	startPos = (int) x;
-	beeMovingR = new Animation(10, tex.beeR[0], tex.beeR[1], tex.beeR[2], tex.beeR[3], tex.beeR[4]);
-	beeMovingL = new Animation(10, tex.beeL[0], tex.beeL[1], tex.beeL[2], tex.beeL[3], tex.beeL[4]);
+	beeMovingR = new Animation(3, tex.beeR[0], tex.beeR[1], tex.beeR[2], tex.beeR[3], tex.beeR[4]);
+	beeMovingL = new Animation(3, tex.beeL[0], tex.beeL[1], tex.beeL[2], tex.beeL[3], tex.beeL[4]);
 }
 
 @Override
 public void render(Graphics g) {
-	if (moving == 1) beeMovingR.drawAnimation(g, (int) x, (int) y-14);
-	else beeMovingL.drawAnimation(g, (int) x, (int) y-14);
+	if (moving == 1) beeMovingR.drawAnimation(g, (int) x, (int) y-20);
+	else beeMovingL.drawAnimation(g, (int) x, (int) y-20);
 }
 
 @Override
 public void tick(ArrayList<GameObject> object) {
-	if (moving == 1) x++;
-	if (moving == -1) x --;
 	
-	if (((int) x) > startPos+250) moving = -1;
+	if ((int) x > startPos+240) moving = -1;
 	if ((int) x < startPos) moving = 1;
 	
-	if (moving == 1) beeMovingR.runAnimation();
-	else beeMovingL.runAnimation();
+	if (moving == 1) x+= 1;
+	if (moving == -1) x -= 1;
+	
+	beeMovingR.runAnimation();
+	beeMovingL.runAnimation();
 }
 
 @Override
 public Rectangle getBounds() {
-	return new Rectangle((int) x, (int) y, 64, 64);
+	return new Rectangle((int) x, (int) y, 32, 32);
 }
 }
